fca24e85bbe8ceb9ae14b7bbb367d7ec21e1c0a7
xiaotao <hwxiaotao@gmail.com>
2021-01-07 20:09:46 +0700

    mod 010703

--- app/src/main/java/com/egglib/xpro/activity/MainActivity.java
+++ app/src/main/java/com/egglib/xpro/activity/MainActivity.java
@@ -19,56 +19,52 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private DaoSession daoSession;
-    private TextView mTvNum;
-    private Button mBtnQuery;
-    private Button mBtnTest;
-    private Button mBtnDrawerLayout;
+    private TextView mTvSqlData;
+    private Button mBtnQuerySql;
+    private Button mBtnTestRecyclerview;
+    private Button mBtnTestDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-        initView();
+        initListener();
     }
 
-    private void initView() {
-        mTvNum = (TextView) findViewById(R.id.tv_num);
-        User user = new User(null, "hello", 10);
+    private void initListener() {
+        User user = new User(null, "hello", 20);
         daoSession = DbOpenHelper.getInstance().getSession();
         long num = daoSession.getUserDao().insert(user);
-        mTvNum.setText("成功插入" + num + "条");
-        mBtnQuery = (Button) findViewById(R.id.btn_query);
-        mBtnQuery.setOnClickListener(this);
-
-        mBtnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TestRecyclerViewActivity.class));
-            }
-        });
-
-        mBtnDrawerLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TestDrawerLayout.class));
-            }
-        });
+        mTvSqlData.setText(String.format("数据库有%s条数据", String.valueOf(num)));
+        mBtnQuerySql.setOnClickListener(this);
+        mBtnTestRecyclerview.setOnClickListener(this);
+        mBtnTestDrawerLayout.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        List<User> userList = daoSession.getUserDao().loadAll();
-        for (User user : userList) {
-            Log.e("111", "id-->" + user.getId() + ",name==" + user.getName() + ",age==" + user.getAge());
+        if (v.getId() == R.id.btn_query_sql) {
+            List<User> userList = daoSession.getUserDao().loadAll();
+            StringBuilder stringBuffer = new StringBuilder();
+            for (User user : userList) {
+                stringBuffer.append("id=").append(user.getId());
+                stringBuffer.append(",name=").append(user.getName());
+                stringBuffer.append(",age=").append(user.getAge());
+                stringBuffer.append("\n");
+            }
+            mTvSqlData.setText(stringBuffer.toString());
+        } else if (v.getId() == R.id.btn_test_recyclerview) {
+            startActivity(new Intent(MainActivity.this, TestRecyclerViewActivity.class));
+        } else if (v.getId() == R.id.btn_test_drawer_layout) {
+            startActivity(new Intent(MainActivity.this, TestDrawerLayout.class));
         }
-        mTvNum.setText("获取到===" + userList.size());
     }
 
     private void initViews() {
-        mTvNum = findViewById(R.id.tv_num);
-        mBtnQuery = findViewById(R.id.btn_query);
-        mBtnTest = findViewById(R.id.btn_test);
-        mBtnDrawerLayout = findViewById(R.id.btn_drawer_layout);
+        mTvSqlData = findViewById(R.id.tv_sql_data);
+        mBtnQuerySql = findViewById(R.id.btn_query_sql);
+        mBtnTestRecyclerview = findViewById(R.id.btn_test_recyclerview);
+        mBtnTestDrawerLayout = findViewById(R.id.btn_test_drawer_layout);
     }
 }
--- app/src/main/java/com/egglib/xpro/activity/TestDrawerLayout.java
+++ app/src/main/java/com/egglib/xpro/activity/TestDrawerLayout.java
@@ -1,7 +1,6 @@
 package com.egglib.xpro.activity;
 
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 
